a1b9f2e0a86b6d4104c1d9acfcc9210705f767da
Christopher Gunter <Christopher Gunter>
2020-08-22 11:38:22 -0700

    Added fork count for each repo also for GitHub.

--- src/pages/api/github/remote/[username]/[githubUsername].tsx
+++ src/pages/api/github/remote/[username]/[githubUsername].tsx
@@ -17,6 +17,7 @@ const createQuery = (username) => {
                 name
                 description
                 pushedAt
+                forkCount
                 stargazers {
                   totalCount
                 }
