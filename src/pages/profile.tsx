6745730e62df13beabc09a31f84800dac16a6d55
Christopher Gunter <Christopher Gunter>
2020-08-27 17:33:51 -0700

    Added integration with Auth.

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MainLayout } from '../Components/layouts/MainLayout';
 import { useFetchUser } from '../utils/user';
+import { Button, Text } from './../../node_modules/@chakra-ui/core';
 import Router from 'next/router';
 
 export default function Profile() {
@@ -18,6 +19,23 @@ export default function Profile() {
       <h1>🤸</h1>
       <p>Welcome to the Profile Page! Here is your profile information:</p>
       <p>{JSON.stringify(user)}</p>
+      <Button
+        onClick={() => {
+          fetch('/api/integration', {
+            credentials: 'same-origin',
+            method: 'POST',
+            body: JSON.stringify({ integrationType: 'github', username: 'cgunter1' }),
+          })
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        }}
+      >
+        <Text>Hello</Text>
+      </Button>
     </>
   );
 }
