b92628b9de222f3fca0bfc3dd7a8d605f6d99360
Cgunter1 <cgunter@ucsc.edu>
2021-04-22 14:48:56 -0700

    feat(integrations): fixed integrations bug for retrieving NPM packages
    The bug was retreiving NPM packages without a NPM or any other integration name on the User's
    Profile would result in a 400 message.

--- src/utils/getUserSettings.ts
+++ src/utils/getUserSettings.ts
@@ -13,7 +13,6 @@ export const getUserSettings = async (
   const { sub } = user;
   console.log(sub);
 
-  // const uri = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.m2hih.gcp.mongodb.net/Atlas?retryWrites=true&w=majority`;
   const uri = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.ehkcd.mongodb.net/HackerStat?retryWrites=true&w=majority`;
 
   const client = await MongoClient.connect(uri, { useNewUrlParser: true });
@@ -22,5 +21,8 @@ export const getUserSettings = async (
     .collection('userProfiles')
     .findOne({ authID: sub }, { [`integration_settings.${integrationType}`]: 1 });
 
+  if (!Object(integrationUsername).hasOwnProperty('integration_settings')) {
+    return { username: '' };
+  }
   return integrationUsername.integration_settings[integrationType];
 };
