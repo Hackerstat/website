47abef4374f7b67754b5f126da3256f2469bc70d
Cgunter1 <cgunter@ucsc.edu>
2021-05-18 18:35:53 -0700

    feat(favicon): added HackerStat Logo favicon

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/static/lightLogo.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
