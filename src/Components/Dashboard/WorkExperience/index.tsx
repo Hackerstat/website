f0751905bf0eeab22fe6c1759479f323dae380b8
Cgunter1 <cgunter@ucsc.edu>
2021-06-04 14:00:24 -0700

    feat(workexperience): workExperienceCard needed to be removed for deployment purposes

--- src/Components/Dashboard/WorkExperience/index.tsx
+++ src/Components/Dashboard/WorkExperience/index.tsx
@@ -1,33 +1,33 @@
-import React, { useState, useEffect, FunctionComponent } from 'react';
-import { Package } from '../../../pages/settings/integrations/add/NPM';
-import Axios from 'axios';
+// import React, { useState, useEffect, FunctionComponent } from 'react';
+// import { Package } from '../../../pages/settings/integrations/add/NPM';
+// import Axios from 'axios';
 
-interface WorkExperienceProps {
-  username: string;
-}
+// interface WorkExperienceProps {
+//   username: string;
+// }
 
-const WorkExperienceCard: FunctionComponent<WorkExperienceProps> = ({ username }) => {
-  const [fetchError, setFetchError] = useState<string>();
-  const [packages, setPackages] = useState<Array<Package>>();
-  const getWorkExperience = async (username) => {
-    try {
-      if (!username) {
-        setFetchError('Required');
-        return;
-      }
-      const result = await Axios.get(`/api/settings/workexperience`, {
-        params: {
-          username: username,
-        },
-      });
-      if (result?.data?.error) {
-        setFetchError(result?.data?.error);
-        throw new Error(result?.data?.error);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-};
+// const WorkExperienceCard: FunctionComponent<WorkExperienceProps> = ({ username }) => {
+//   const [fetchError, setFetchError] = useState<string>();
+//   const [packages, setPackages] = useState<Array<Package>>();
+//   const getWorkExperience = async (username) => {
+//     try {
+//       if (!username) {
+//         setFetchError('Required');
+//         return;
+//       }
+//       const result = await Axios.get(`/api/settings/workexperience`, {
+//         params: {
+//           username: username,
+//         },
+//       });
+//       if (result?.data?.error) {
+//         setFetchError(result?.data?.error);
+//         throw new Error(result?.data?.error);
+//       }
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+// };
 
-export default WorkExperienceCard;
+// export default WorkExperienceCard;
