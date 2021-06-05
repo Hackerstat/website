4ea0e1bcb879d094d71b1f0db07c274c4b40d589
LouisIV <Louislombardoiv@gmail.com>
2021-06-04 18:23:13 -0700

    fix: 🐛 Changed VERCEL URL

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,7 @@
 import { initAuth0 } from '@auth0/nextjs-auth0';
 
 export default initAuth0({
-  baseURL: process.env.VERCEL_URL || 'http://localhost:3000',
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000',
   issuerBaseURL: 'https://slack-clone-auth0.auth0.com',
   clientID: process.env.clientId,
   clientSecret: process.env.clientSecret,
